10ff5f8870d90596205eb7a3277198980f2653d4
xiezhongdong <xiezhongdong@leyaoyao.org>
2018-01-20 14:57:42 +0800

    改全局域名

--- src/main/resources/static/page/js/config/app_config.js
+++ src/main/resources/static/page/js/config/app_config.js
@@ -1,4 +1,4 @@
 var memberApp = angular.module('memberApp');
 memberApp.run(['$rootScope', function($rootScope){
-    $rootScope.contextPath = 'http://localhost:8801/member';
+    $rootScope.contextPath = 'http://120.79.81.123:8801/member/';
 }]);
